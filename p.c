eea357e2bbc660bb624b15611028ebdffb814d19
Deepak Choudhary <deepak201001@gmail.com>
2012-11-11 21:42:42 +0530

    working part 1

--- p.c
+++ p.c
@@ -7,6 +7,8 @@ struct node *next;
 };
 
 struct node **p, **q;
+int *visited;
+int *high_pt;
 
 void create_head_pointers(int n)
 {
@@ -14,10 +16,10 @@ void create_head_pointers(int n)
 	p = (struct node **) malloc (n * sizeof(struct node *));
 	q = (struct node **) malloc (n * sizeof(struct node *));
 	
-	for(i=0;i<n;i++)
+	for(i=0; i<n; i++)
 	{
-		p[i]=NULL;
-		q[i]=NULL;
+		p[i] = NULL;
+		q[i] = NULL;
 	}
 
 }
@@ -27,26 +29,114 @@ void edge(int a, int b)
 	struct node *temp;
 	temp = (struct node *) malloc (sizeof(struct node));
 
-	if(p[a]==NULL)
+	temp->index = b;
+	temp->next = NULL;
+
+	if(q[a] == NULL)
 	{
-		
-		p[a]=temp;
+		p[a] = temp;
+		q[a]=temp;
 	}
+	else
+	{
+		q[a] -> next = temp;
+		q[a] = temp;
+	}
+}
+
+void dfs(int i)
+{
+	visited[i] = 1;
+	struct node *temp;
+	temp = p[i];
+
+	while(temp != NULL)
+	{
+		if(visited[ temp -> index ] == 0)
+		{
+			dfs(temp -> index);	
+		}
+		temp = temp -> next;
+	}
+}
+
+void printdfs(int i)
+{
+	visited[i] = 1;
+	printf("%d,",i);
+	struct node *temp;
+        temp = p[i];
+
+        while(temp != NULL)
+        {
+                if(visited[ temp -> index ] == 0)
+                {
+                        printdfs(temp -> index);      
+                }
+		temp = temp -> next;
+        }
+
+}
+
+
+
+void dfs_traversal(int n)
+{
+	int i, count=0;
+	
+	for(i=0;i<n;i++)
+	{
+		if(visited[i] == 0)
+		{
+			dfs(i);
+			count++;
+		}
+	}
+	
+	printf("There are %d components of the graph. These are\n",count);
+
+	for(i=0; i<n ;i++)
+	visited[i] = 0;
+
+	for(i=0 ; i<n ; i++)
+	{
+		if(visited[i] == 0)
+		{
+			printf("{");
+			printdfs(i);
+			printf("\b} , ");
+		}
+	}
+	printf("\b\b\b\n");
+	
+	for(i=0 ; i<n; i++)
+	visited[i] == 0;
+
+	
 }
 
 int main()
 {
 	int n,m;
 	int i;
+	int a,b;
+	struct node *temp;
 
 	scanf("%d %d",&n,&m);
 	create_head_pointers(n);
-
+	
+	visited = (int *) malloc (n * sizeof(int));
 	for(i=0;i<n;i++)
-	if(p[i]==NULL)
+		visited[i]=0;
+
+	struct
+	for(i=0;i<m;i++)
 	{
-		printf("%d OK\n",i);
+		scanf("%d%d",&a,&b);
+		edge(a,b);
+		edge(b,a);
 	}
 
+	dfs_traversal(n);
 	return 0;
 }
